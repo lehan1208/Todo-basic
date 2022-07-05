2ffbf8452921d9feb0f4d5fae58cba91ed2cfdd9
Han <101984174+HaanLee@users.noreply.github.com>
2022-07-05 17:08:51 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import TodoForm from './components/TodoForm';
 
 function App() {
-    return (
-        <div className="todo-app">
-            <TodoForm />
-        </div>
-    );
+  return (
+    <div className="todo-app">
+      <TodoForm />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,16 +4,34 @@ import React from 'react';
 function TodoForm() {
   const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
-  const [todoEditing, setTodoEditing] = useState(null);
+  const [todoEditing, setTodoEditing] = useState({});
   const [editingText, setEditingText] = useState('');
 
-  useEffect(() => {
-    const temp = localStorage.getItem('todoList');
-    const loadedTodoList = JSON.parse(temp);
+  const fetchData = () => {
+    fetch('https://6293b734089f87a57ac4de6d.mockapi.io/list', {
+      method: 'GET', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Success:', data);
+        setTodoList(data);
+      })
+      .catch((error) => {
+        setTodoList([]);
+        console.error('Error:', error);
+      });
+  };
 
-    if (loadedTodoList) {
-      setTodoList(loadedTodoList);
-    }
+  useEffect(() => {
+    // const temp = localStorage.getItem('todoList');
+    // const loadedTodoList = JSON.parse(temp);
+    fetchData();
+    // if (loadedTodoList) {
+    //   setTodoList(loadedTodoList);
+    // }
   }, []);
 
   useEffect(() => {
@@ -25,44 +43,92 @@ function TodoForm() {
     e.preventDefault();
 
     const newTodo = {
-      // Tạo một object newTodo chứa id, text, và trạng thái complete
-      id: new Date().getTime(),
-      text: todo,
-      completed: false,
+      content: todo,
+      status: false,
     };
 
-    setTodoList([...todoList].concat(newTodo)); // Nối newTodo vào mảng todoList ban đầu
-    setTodo('');
+    // setTodoList([...todoList].concat(newTodo)); // Nối newTodo vào mảng todoList ban đầu
+    fetch('https://6293b734089f87a57ac4de6d.mockapi.io/list', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        fetchData();
+        setTodo('');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   const deleteTodo = (id) => {
-    const newTodoList = [...todoList].filter((todo) => todo.id !== id);
-    setTodoList(newTodoList);
+    fetch(`https://6293b734089f87a57ac4de6d.mockapi.io/list/${id}`, {
+      method: 'DELETE', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        fetchData();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
-  const toggleComplete = (id) => {
-    const updatedTodoList = [...todoList].map((todo) => {
-      // map qua mảng todoList
-      if (todo.id === id) {
-        // Nếu click vào id giống với id của todo
-        todo.completed = !todo.completed; // => chuyển trạng thái completed của todo đó
-      }
-      return todo;
-    });
-
-    setTodoList(updatedTodoList);
+  const toggleComplete = (todo) => {
+    const editTodo = {
+      ...todo,
+      status: !todo.status,
+    };
+    fetch(`https://6293b734089f87a57ac4de6d.mockapi.io/list/${editTodo.id}`, {
+      method: 'PUT', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editTodo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        fetchData();
+        setTodoEditing({});
+        setEditingText('');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
-  const editTodo = (id) => {
-    const updatedTodoList = [...todoList].map((todo) => {
-      if (todo.id === id) {
-        todo.text = editingText;
-      }
-      return todo;
-    });
-    setTodoList(updatedTodoList);
-    setTodoEditing(null);
-    setEditingText('');
+  const editTodo = () => {
+    const editTodo = {
+      ...todoEditing,
+      content: editingText,
+    };
+    fetch(`https://6293b734089f87a57ac4de6d.mockapi.io/list/${editTodo.id}`, {
+      method: 'PUT', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editTodo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        fetchData();
+        setTodoEditing({});
+        setEditingText('');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   return (
@@ -89,11 +155,11 @@ function TodoForm() {
           <div className="todo-text">
             <input
               type="checkbox"
-              id="completed"
-              checked={todo.completed}
-              onChange={() => toggleComplete(todo.id)}
+              id="status"
+              checked={todo.status}
+              onChange={() => toggleComplete(todo)}
             />
-            {todoEditing === todo.id ? (
+            {todoEditing.id === todo.id ? (
               <input
                 type="text"
                 value={editingText}
@@ -102,14 +168,21 @@ function TodoForm() {
                 }}
               />
             ) : (
-              <div>{todo.text}</div>
+              <div>{todo.content}</div>
             )}
           </div>
           <div className="todo-actions">
-            {todoEditing === todo.id ? (
-              <button onClick={() => editTodo(todo.id)}>Submit Edits</button>
+            {todoEditing.id === todo.id ? (
+              <button onClick={() => editTodo()}>Save</button>
             ) : (
-              <button onClick={() => setTodoEditing(todo.id)}>Edit Todo</button>
+              <button
+                onClick={() => {
+                  setEditingText(todo.content);
+                  setTodoEditing(todo);
+                }}
+              >
+                Edit Todo
+              </button>
             )}
             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
           </div>
